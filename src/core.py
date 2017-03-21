75caad44941acea2a8fc731a94108d94f91e8c8c
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-03-21 20:37:25 -0300

    Initial menu finished. New char menu started

--- src/core.py
+++ src/core.py
@@ -3,28 +3,43 @@ import menu_controller
 
 
 class game_runner():
+    """
+        Class used to run an instace of a game. This class controls all the
+    game, including transitions among the different scenes and menus.
+    """
     def __init__(self):
+        self.scenes = {
+            'main_menu': self.main_menu,
+            'choose_char': self.choose_char,
+        }
+        self.actual_character = {}
         self.screen_settings = {}
         self.screen_settings['size'] = (960, 440)
         self.screen_settings['color'] = (250, 250, 250)
         self.screen_settings['caption'] = 'pyDino'
+        # Initial setup
+        self.actual_scene = 'main_menu'
+        self.game_running = 1
+
+    def main_menu(self):
+        menu = menu_controller.menu(self.screen, self.screen_settings)
+        self.game_running, self.actual_scene = menu.show_initial_menu()
+
+    def choose_char(self):
+        menu = menu_controller.menu(self.screen, self.screen_settings)
+        self.game_running, self.actual_scene, props = menu.show_choose_char()
+        if (props is not None):
+            self.actual_character['name'] = props['name']
 
     def run(self):
         # Displays the window
         self.screen = pygame.display.set_mode(self.screen_settings['size'])
         pygame.display.set_caption(self.screen_settings['caption'])
 
-        actual_scene = 'main_menu'
-        game_running = 1
-
-        while game_running:
+        while self.game_running:
             for event in pygame.event.get():
                 if (event.type == pygame.QUIT):
                     return 0
 
-            if (actual_scene == 'main_menu'):
-                menu = menu_controller.menu(self.screen, self.screen_settings)
-                game_running = menu.show_initial_menu()
-
-            # Updates the game display
-            pygame.display.flip()
+            # Calls the right menu to be executed
+            self.scenes[self.actual_scene]()
--- src/menu_controller.py
+++ src/menu_controller.py
@@ -3,30 +3,56 @@ from utils import *
 
 class menu():
     def __init__(self, surface, screen_settings):
-        self.resouces = {}
+        self.resources = {}
         self.screen = surface
         self.screen_size = screen_settings['size']
         self.background_color = screen_settings['color']
         self.caption = screen_settings['caption']
-        self.resouces['ground'] = load_image('ground.png')
-        self.last_mouse_click = (-1, -1)
+        self.resources['ground'] = load_image('ground.png')
 
     def show_initial_menu(self):
+        # Setup
         pygame.display.set_caption(self.caption + ': Main Menu')
+        self.last_mouse_click = (-1, -1)
+        new_game_pos = (40, 40)
+        new_game_size = (159, 29)
         while True:
             for event in pygame.event.get():
                 if (event.type == pygame.QUIT):
-                    return 0
+                    return (0, '')
                 if (event.type == pygame.MOUSEBUTTONDOWN):
                     self.last_mouse_click = pygame.mouse.get_pos()
-            if 40 <= self.last_mouse_click[0] <= (40 + 159):
-                if 40 <= self.last_mouse_click[1] <= (40 + 29):
+
+            # New game button
+            if (new_game_pos[0] <= self.last_mouse_click[0] <=
+                    new_game_pos[0] + new_game_size[0]):
+                if (new_game_pos[1] <= self.last_mouse_click[1] <=
+                        new_game_pos[1] + new_game_size[1]):
                     print('Botão de iniciar novo jogo')
                     self.last_mouse_click = (-1, -1)
+                    return (1, 'choose_char')
 
             self.screen.fill(self.background_color)
-            self.screen.blit(self.resouces['ground'], (0, self.screen_size[1] - 30))
-            self.screen.blit(load_image('new_game_button.png'), (40, 40))
+            self.screen.blit(self.resources['ground'], (0, self.screen_size[1] - 30))
+            self.screen.blit(load_image('new_game_button.png'), new_game_pos)
+
+            # Updates the game display
+            pygame.display.flip()
+
+    def show_choose_char(self):
+        # Setup
+        pygame.display.set_caption(self.caption + ': Choose Character')
+        self.last_mouse_click = (-1, -1)
+        char_properties = {}
+        while True:
+            for event in pygame.event.get():
+                if (event.type == pygame.QUIT):
+                    return (0, '', None)
+                if (event.type == pygame.MOUSEBUTTONDOWN):
+                    self.last_mouse_click = pygame.mouse.get_pos()
+
+            self.screen.fill(self.background_color)
+            self.screen.blit(self.resources['ground'], (0, self.screen_size[1] - 30))
 
             # Updates the game display
             pygame.display.flip()
