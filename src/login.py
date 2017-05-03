460969fecc4e7b7f4bceaeaa406a51e81da8254f
macelai <vini.macelai@hotmail.com>
2017-05-03 09:09:01 -0300

    Tk added

--- src/login.py
+++ src/login.py
@@ -1,4 +1,5 @@
 import pygame
+from tkinter import *
 
 class login():
     def __init__(self, surface, screen_seetings):
@@ -10,22 +11,64 @@ class login():
         self.resources['ground'] = self.load_image('ground.png')
 
     def show(self):
-        pygame.display.set_caption(self.caption + ': Login')
-        self.last_mouse_click = (-1, -1)
-        login_name_pos = (self.screen_size[0]*0.45, self.screen_size[1]*0.35)
-        login_pass_pos = (self.screen_size[0]*0.45, self.screen_size[1]*0.7)
-        login_size = (0, 0)
+        root = Tk()
+        self.input_box(root)
         while True:
-            for event in pygame.event.get():
-                if (event.type == pygame.QUIT):
-                    return (0, '')
-                if (event.type == pygame.MOUSEBUTTONDOWN):
-                    self.last_mouse_click = pygame.mouse.get_pos()
-
-            self.screen.fill(self.background_color)
-            self.screen.blit(self.resources['ground'], (0, self.screen_size[1] - 30))
-
             pygame.display.flip()
+            root.update()
 
     def load_image(self, filename):
         return pygame.image.load(str('../resources/' + filename))
+
+    def input_box(self, master=None):
+        self.fontePadrao = ("Arial", "10")
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer.pack()
+   
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["padx"] = 20
+        self.segundoContainer.pack()
+   
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["padx"] = 20
+        self.terceiroContainer.pack()
+   
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 20
+        self.quartoContainer.pack()
+   
+        self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
+        self.titulo["font"] = ("Arial", "10", "bold")
+        self.titulo.pack()
+   
+        self.nomeLabel = Label(self.segundoContainer,text="Nome", font=self.fontePadrao)
+        self.nomeLabel.pack(side=LEFT)
+   
+        self.nome = Entry(self.segundoContainer)
+        self.nome["width"] = 30
+        self.nome["font"] = self.fontePadrao
+        self.nome.pack(side=LEFT)
+   
+        self.senhaLabel = Label(self.terceiroContainer, text="Senha", font=self.fontePadrao)
+        self.senhaLabel.pack(side=LEFT)
+   
+        self.senha = Entry(self.terceiroContainer)
+        self.senha["width"] = 30
+        self.senha["font"] = self.fontePadrao
+        self.senha["show"] = "*"
+        self.senha.pack(side=LEFT)
+   
+        self.autenticar = Button(self.quartoContainer)
+        self.autenticar["text"] = "Autenticar"
+        self.autenticar["font"] = ("Calibri", "8")
+        self.autenticar["width"] = 12
+        self.autenticar["command"] = self.return_menu
+        self.autenticar.pack()
+   
+        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
+        self.mensagem.pack()
+
+    def return_menu(self, master=None):
+        master.destroy()
+        return(1, 'main_menu')
