122d3eb8b6070f3c75b0fa09c14398188eb05da8
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-06-10 11:05:13 -0300

    Initial approach, using SQLite

---
+++ src/database.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+
+class Database():
+    """
+            A class tha represents a database. The database engine used is the
+            SQLite.
+    """
+    def __init__(self, db_name='players_database.db'):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        """
+            Creates a table inside the database.
+        """
+        try:
+            self.cursor.execute('''CREATE TABLE players
+                               (name TEXT PRIMARY KEY, type TEXT, coins INTEGER)
+                               ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            return "Table already exists."
+
+    def add(self, values):
+        """
+            Adds a row with the given values to the table.
+        """
+        self.cursor.execute('''INSERT INTO players(name, type, coins)
+                            VALUES(:name, :type, :coins)''',
+                            values)
+        self.connection.commit()
+
+    def close(self):
+        """
+            Closes the connection with the database.
+        """
+        self.connection.commit()
+        self.connection.close()
+
+    def execute(self, command):
+        """
+            Executes a given query in the database.
+        """
+        return self.cursor.execute(command)
